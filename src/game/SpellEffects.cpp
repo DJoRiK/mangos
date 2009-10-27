4f69eb0b1f993df12c9308152cf9c86dade79202
Insider <insider42rus@yandex.ru>
2009-10-27 16:23:52 +0700

    Temporary reverted [patch 83]

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -1718,11 +1718,6 @@ void Spell::EffectDummy(uint32 i)
                     m_caster->CastSpell(m_caster, 45182, true);
                     return;
                 }
-                case 51662:                                 // Hunger for Blood
-                {
-                    m_caster->CastSpell(m_caster, 63848, true);
-                    return;
-                }
             }
             break;
         case SPELLFAMILY_HUNTER:
