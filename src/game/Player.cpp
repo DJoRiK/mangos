4b258fe79bf0e4a28a27e9826788f1b827614b2f
Insider <insider42rus@yandex.ru>
2009-10-29 21:53:07 +0700

    Reverted [patch 189]

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -13463,7 +13463,7 @@ void Player::KilledMonster( CreatureInfo const* cInfo, uint64 guid )
 void Player::KilledMonsterCredit( uint32 entry, uint64 guid )
 {
     uint32 addkillcount = 1;
-    GetAchievementMgr().UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_KILL_CREATURE, guid ? GetMap()->GetCreature(guid)->GetEntry() : entry, addkillcount);
+    GetAchievementMgr().UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_KILL_CREATURE, entry, addkillcount);
     for( int i = 0; i < MAX_QUEST_LOG_SIZE; ++i )
     {
         uint32 questid = GetQuestSlotQuestId(i);
