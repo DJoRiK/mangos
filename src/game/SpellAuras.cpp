3f40e5fc59df27acbfee59abc99a80c2f2c55aa1
Insider <insider42rus@yandex.ru>
2009-11-06 19:20:56 +0700

    [patch 207] Fixed spell 34471 applying from 34692

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -4515,7 +4515,7 @@ void Aura::HandleModMechanicImmunity(bool apply, bool /*Real*/)
         if (Unit* owner = m_target->GetOwner())
         {
             // Search talent
-            Unit::AuraList const& dummyAuras = owner->GetAurasByType(SPELL_AURA_DUMMY);
+            Unit::AuraList const& dummyAuras = owner->GetAurasByType(SPELL_AURA_MOD_DAMAGE_PERCENT_DONE);
             for(Unit::AuraList::const_iterator i = dummyAuras.begin(); i != dummyAuras.end(); ++i)
             {
                 if ((*i)->GetSpellProto()->SpellIconID == 2229)
