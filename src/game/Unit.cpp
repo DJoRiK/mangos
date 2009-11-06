5d7714ae6c75adea0d023332a2c81a2ea12b69d6
Insider <insider42rus@yandex.ru>
2009-11-06 20:42:51 +0700

    [patch 208] Fixed tick damage increasing for spell 53380 and ranks for more than one critical strike while previous debuff from that spell is present on the target

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -6016,7 +6016,31 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, Aura* triggeredByAu
             if (dummySpell->SpellIconID == 3025)
             {
                 // 4 damage tick
-                basepoints0 = triggerAmount*damage/400;
+                int32 TickCD = 3000;
+                Aura const* PrevAura = NULL;
+
+                Unit::AuraList const &mPeriodic = target->GetAurasByType(SPELL_AURA_PERIODIC_DAMAGE);
+                for(Unit::AuraList::const_iterator i = mPeriodic.begin(); i != mPeriodic.end(); ++i)
+                {
+                    if ((*i)->GetCasterGUID() != this->GetGUID())
+                        continue;
+
+                    if ((*i)->GetSpellProto()->SpellIconID == 3025)
+                    {
+                        PrevAura = *i;
+                        break;
+                    }
+                }
+
+                if (PrevAura)
+                {
+                    int32 PrevTickDmg = SpellDamageBonus(target, PrevAura->GetSpellProto(), PrevAura->GetModifier()->m_amount, DOT);
+                    int32 RemainingTicks = 1 + PrevAura->GetAuraDuration() / TickCD;
+                    basepoints0 = RemainingTicks * PrevTickDmg;
+                }
+
+                basepoints0 = (basepoints0 +  triggerAmount * damage * 0.01) / 4;
+
                 triggered_spell_id = 61840;
                 break;
             }
