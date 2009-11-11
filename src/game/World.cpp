46ee70f187f55e3ef5d0970528c93b30baf55498
Insider <insider42rus@yandex.ru>
2009-11-11 16:51:31 +0700

    Added new version of [patch 130]

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -63,6 +63,7 @@
 #include "WaypointManager.h"
 #include "GMTicketMgr.h"
 #include "Util.h"
+#include "AuctionHouseBot.h"
 
 INSTANTIATE_SINGLETON_1( World );
 
@@ -1532,6 +1533,8 @@ void World::SetInitialWorldSettings()
     uint32 nextGameEvent = sGameEventMgr.Initialize();
     m_timers[WUPDATE_EVENTS].SetInterval(nextGameEvent);    //depend on next event
     sLog.outString("Starting Autobroadcast system by Xeross..." );
+    sLog.outString("Initialize AuctionHouseBot...");
+    auctionbot.Initialize();
     sLog.outString( "WORLD: World initialized" );
 
     uint32 uStartInterval = getMSTimeDiff(uStartTime, getMSTime());
@@ -1604,6 +1607,7 @@ void World::Update(uint32 diff)
     /// <ul><li> Handle auctions when the timer has passed
     if (m_timers[WUPDATE_AUCTIONS].Passed())
     {
+        auctionbot.Update();
         m_timers[WUPDATE_AUCTIONS].Reset();
 
         ///- Update mails (return old mails with item, or delete them)
